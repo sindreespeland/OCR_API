d6e20c2e6e96f4cbc26c6c9a659d0d737e8b3ace
Sindre Espeland <sindre.espeland2001@gmail.com>
2025-01-02 15:01:38 +0100

    OCR app finished

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # OCR_API
 An api that provide OCR functionality
+
+## Install tesseract on mac
+```bash
+brew install tesseract
+```
+
+## Create venv
+```bash
+python3 -m venv venv
+```
+```bash
+source venv/bin/activate
+```
---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, File, UploadFile
+import shutil
+import pytesseract
+
+app = FastAPI()
+
+@app.post("/ocr")
+def ocr(image: UploadFile = File(...)):
+    file_path = 'txtFile'
+    with open(file_path, 'w+b') as buffer:
+        shutil.copyfileobj(image.file, buffer)
+    return pytesseract.image_to_string(file_path, lang='eng')
